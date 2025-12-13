5626439aaced5c88ff4f59a39557b81f0f7a0200
radiantblue99 <javiersalman99@gmail.com>
2025-12-13 10:03:53 +0700

    versi 3

--- signin.php
+++ signin.php
@@ -2,42 +2,52 @@
 session_start();
 include 'konek.php';
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$query = mysqli_query($conn, "SELECT * FROM users WHERE username='$username'");
-$user = mysqli_fetch_assoc($query);
+    $query = mysqli_query($conn, "SELECT * FROM users WHERE username='$username'");
+    $user = mysqli_fetch_assoc($query);
 
-if ($user && password_verify($password, $user['password'])) {
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['fullname'] = $user['fullname'];
+        $_SESSION['role'] = $user['role'];
 
-    $_SESSION['username'] = $user['username'];
-    $_SESSION['fullname'] = $user['fullname'];
-    $_SESSION['role'] = $user['role'];
-
-    header("Location: ../home.php");
-} else {
-    echo "Username atau password salah!";
+        header("Location: home.php");
+        exit;
+    } else {
+        $error = "Username atau password salah!";
+    }
 }
 ?>
 
-
 <!DOCTYPE html>
+<html>
 <head>
   <link href="https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="style.css">
   <title>Sign In</title>
 </head>
 <body>
-  <div class="form-box">
-    <h1>Sign In</h1>
 
-    <div class="input-field">Username</div>
-    <div class="input-field">Password</div>
+<div class="form-box">
+  <h1>Sign In</h1>
 
-    <p style="margin-top:20px;">
-      Belum punya akun?
-      <a href="signup.html" class="link">Daftar sekarang</a>
-    </p>
-  </div>
+  <?php if (isset($error)) echo "<p style='color:red'>$error</p>"; ?>
+
+  <form method="POST">
+    <input type="text" name="username" placeholder="Username" class="input-field" required>
+    <input type="password" name="password" placeholder="Password" class="input-field" required>
+
+    <button type="submit" name="login">Login</button>
+  </form>
+
+  <p style="margin-top:20px;">
+    Belum punya akun?
+    <a href="signup.php" class="link">Daftar sekarang</a>
+  </p>
+</div>
+
 </body>
 </html>
